96bedadc4f399ff60f25b33a0480e2ce47f851fe
chakrikanethi <chakrikachowdary76@gmail.com>
2025-05-20 23:55:42 +0530

    update

---
+++ a9/script.js
@@ -0,0 +1,14 @@
+  
+const fruits = ["apple", "banana", "mango", "grapes", "orange", "kiwi"];
+
+
+const checkAvailability = () => {
+  const input = document.getElementById("fruitInput").value.trim().toLowerCase();
+  const result = document.getElementById("result");
+
+  result.textContent = fruits.includes(input)
+    ? `${input} is available!`
+    : `${input} is not available!!`;
+};
+
+document.getElementById("checkBtn").addEventListener("click", checkAvailability);
